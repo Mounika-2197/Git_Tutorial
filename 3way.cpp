6fef209b438b9f64572a5a9886d61e0daffdca34
Mounika Yakasiri <55370272+Mounika-2197@users.noreply.github.com>
2022-03-22 13:11:55 -0700

    added name

--- 3way.cpp
+++ 3way.cpp
@@ -2,9 +2,7 @@
 using namespace std;
 
 int main(){
-    auto a = 10, b = 5.5;
-    auto comp = a<=>b < 0;
-    if(!comp){
-        cout << "yaayyy Mounika Yakasiri Reddy!!" << endl;
+    string name = "Mounika";
+    cout << "My name is " << name << endl;
     }
 }
